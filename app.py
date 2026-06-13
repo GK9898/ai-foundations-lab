ce8471e83e7e994a8218363d37bc2c91e704b8bc
GK9898 <gulshan.sahu68@gmail.com>
2026-06-13 15:52:53 +0530

    code from second lecture.

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print("hello")
+users_database = [
+
+{"id": 101, "name": "Alice", "role": "admin", "is_active": True},
+
+{"id": 102, "name": "Bob", "role": "user", "is_active": False},
+
+{"id": 103, "name": "Charlie", "role": "editor", "is_active": True}
+
+]
+
+active_user = [user['name'] for user in users_database if user["is_active"] == True]
+print(active_user)
